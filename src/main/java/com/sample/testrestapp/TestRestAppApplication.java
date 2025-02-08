3a03f036a61f467cd09a7eadd043d8b3495ca006
hskvasa <88998221+hskvasa@users.noreply.github.com>
2025-02-08 11:47:37 -0500

    Update TestRestAppApplication.java

--- src/main/java/com/sample/testrestapp/TestRestAppApplication.java
+++ src/main/java/com/sample/testrestapp/TestRestAppApplication.java
@@ -8,6 +8,7 @@ public class TestRestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestRestAppApplication.class, args);
+		System.out.println("Test commit");
 	}
 
 }
